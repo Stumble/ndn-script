77a36cf16256cca25d764a11181b784847597132
Yumin Xia <me@yumin-xia.com>
2016-09-05 00:36:55 +0800

    add debian script

---
+++ install-on-Debian.sh
@@ -0,0 +1,18 @@
+# sudo apt-get install -y build-essential libcrypto++-dev libsqlite3-dev libboost-all-dev
+git clone https://github.com/named-data/ndn-cxx
+cd ndn-cxx
+./waf configure --with-examples
+./waf
+sudo ./waf install
+sudo ldconfig
+
+cd ..
+
+git clone --recursive https://github.com/named-data/NFD
+cd NFD
+./waf configure --without-websocket --without-libpcap
+./waf
+sudo ./waf install
+sudo ldconfig
+
+sudo cp /usr/local/etc/ndn/nfd.conf.sample /usr/local/etc/ndn/nfd.conf
