367c58d936274cc104e688838ad419186ae4a7b6
Yumin Xia <me@yumin-xia.com>
2017-06-07 02:19:50 +0000

    Update Pi's script

--- install-on-Raspberry.sh
+++ install-on-Raspberry.sh
@@ -1,6 +1,7 @@
-sudo apt-get install -y build-essential libcrypto++-dev libsqlite3-dev libboost-all-dev
+sudo apt-get install -y build-essential libcrypto++-dev libsqlite3-dev libboost-all-dev libssl-dev
 git clone https://github.com/named-data/ndn-cxx
 cd ndn-cxx
+git checkout ndn-cxx-0.5.1
 ./waf configure --with-examples --boost-lib /usr/lib/arm-linux-gnueabihf/
 ./waf
 sudo ./waf install
@@ -10,6 +11,7 @@ cd ..
 
 git clone --recursive https://github.com/named-data/NFD
 cd NFD
+git checkout NFD-0.5.1
 ./waf configure --without-websocket --without-libpcap --boost-lib /usr/lib/arm-linux-gnueabihf/
 ./waf
 sudo ./waf install
